3ea1ca524290711ea6e5f0bd82cb0f8821a75a23
unknown <lvxiaobao_fbi@163.com>
2015-05-23 22:51:54 +0800

    m

--- js/front/ucenter.js
+++ js/front/ucenter.js
@@ -3,6 +3,12 @@ zhongchou.ucenter = {
 	//初始化项目进展页
 	initProcessPage : function(){
 		this.uploadImg();
+		this.initListeners();
+	},
+	//初始化问题管理页面
+	initQuestion : function(){
+		this.replyTextarea($('.limit'));
+		this.initListeners();
 	},
 	uploadImg : function(){
 		var conf = {
@@ -66,6 +72,90 @@ zhongchou.ucenter = {
 		uploader.on( 'uploadError', function( file ) {
 		    $('#'+file.id).remove();
 		    alert('上传失败请重试！');
+		});
+	},
+	replyTextarea : function(item){
+		item.limitTextarea({
+			maxNumber:200
+		});
+	},
+	initListeners : function(){
+		var _this = this;
+		//删除已上传图片，项目进展页面
+		$(document.body).on('click', '.delimg', function(){
+			var $this = $(this);
+			var input = $this.closest('.uppanel').find('input[type="hidden"]');
+			var uppanel = $this.closest('.uppanel');
+			//删除元素
+			$this.closest('.file-item').remove();
+			//为表单重新赋值
+			var newval = [];
+			uppanel.find('.file-item').each(function(){
+				newval.push($(this).data('url'));
+			});
+			input.val(newval.join(','));
+		});
+
+		//问题管理页面,点击回复
+		$(document.body).on('click', '.reply', function(){
+			$(this).closest('.commentitem').find('.replypanel').slideDown(200);
+		});
+
+		//问题管理，取消回复
+		$(document.body).on('click', '.cancelreply', function(){
+			$(this).closest('.commentitem').find('.replypanel').slideUp(200);
+		});
+
+		//问题管理，保存回复
+		$(document.body).on('click', '.savereply', function(){
+			var item = $(this).closest('.commentitem');
+			var text = item.find('.replytext').val();
+			if(text==''){
+				alert('请输入内容！');
+				return;
+			}
+			if(text.length>200){
+				alert('内容不能超过200字');
+				return;
+			}
+			var id = item.data('id');
+			//提交成功后的回调函数，传入名字和内容
+			var callback = function(name, content){
+				item.find('.replycontent').html('<h3 class="ownername">'+name+'</h3><div class="ownerreply">'+content+'</div>');
+				item.find('.reply').hide();
+				item.find('.deletereply').show();
+			}
+			//发起请求
+			$.ajax({
+				url : '',
+				data : {},
+				success : function(data){
+					callback('大樱桃专卖店', text);
+				}
+			});
+		});
+
+		//问题管理，删除回复
+		$(document.body).on('click', '.deletereply', function(){
+			var item = $(this).closest('.commentitem');
+			var id = item.data('id');
+			//删除成功的回调函数
+			var callback = function(){
+				var replycontent = item.find('.replycontent');
+				replycontent.html('<textarea class="replytext limit" placeholder="回复内容请在200字以内"></textarea><div class="mt10"><a href="javascript:void(0);" class="savereply bluebtn mr10">保存</a><a href="javascript:void(0);" class="cancelreply cbtn mr10">取消</a></div>');
+				_this.replyTextarea(replycontent.find('.limit'));
+				item.find('.replypanel').hide();
+				item.find('.reply').show();
+				item.find('.deletereply').hide();
+			}
+			//发起请求
+			$.ajax({
+				url : '',
+				data : {},
+				success : function(){
+					callback();
+				}
+			});
 		});
 	}
 }
